9931ef2223c01945f177b69defdc7d50ecea8b70
CharrafiMed <mhch5003@gmail.com>
2025-03-19 12:58:45 +0100

    copies existing work

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,116 @@
+<section class="mx-auto max-w-7xl px-6 lg:px-8">
+    {{-- BAKGROUND GRID --}}
+    <x-grid-svg class="absolute inset-0 -z-0 h-full w-full !text-red-500 [mask-image:linear-gradient(to_top,transparent,white)]"
+                gridSize="40"
+                lineColor="gray"
+                lineOpacity="0.15"
+                backgroundColor="transparent"
+                :highlightedRow="7"
+                animationDirection="rtl"
+                animationDuration="6"
+                highlightColorStart="red"
+                highlightColorEnd="blue"
+                strokeWidth="1" />
+
+    {{-- SECTION CONTENT --}}
+
+    <div class="relative mx-auto max-w-3xl pt-[7rem] text-center">
+        <h1 class="bg-gradient-to-br from-white to-zinc-500 bg-clip-text py-3 text-4xl/[1.07] font-bold tracking-tight text-transparent md:text-5xl/[1.07]"
+            style="opacity: 1; transform: none;">Streamline Your Documentation Workflow with <span
+                  class="text-sky-500">Converge</span></h1>
+
+        <p class="mt-6 text-lg font-medium text-zinc-400 md:text-xl"
+           style="opacity: 1; transform: none;">Converge is the open-source framework that empowers anyone to build
+            high-quality, dynamic docs from Markdown. Developer-friendly. Intuitive. And designed for teams who care
+            about clarity
+        </p>
+
+        <div class="mt-10 flex flex-col items-center justify-center gap-y-8">
+            <div style="opacity: 1; transform: none;">
+                <button class="hover:shadow-glow group relative rounded-full p-px text-sm/6 text-zinc-400 duration-300 hover:text-zinc-100"
+                        type="button"
+                        aria-haspopup="dialog"
+                        aria-expanded="false"
+                        aria-controls="radix-:Rjlj9ukq:"
+                        data-state="closed">
+                    <span class="absolute inset-0 overflow-hidden rounded-full">
+                        <span
+                              class="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+                        </span>
+                    </span>
+                    <div class="relative z-10 rounded-full bg-zinc-950 px-4 py-1.5 ring-1 ring-white/10">
+                        Get started
+                    </div>
+                    <span
+                          class="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40">
+                    </span>
+                </button>
+            </div>
+
+            <div class="group"
+                 style="opacity: 1;"><a class="flex flex-col items-center gap-1"
+                   href="/#intro">
+                    <p class="text-sm/6 text-zinc-400 duration-300 group-hover:text-zinc-100">Learn more</p>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         width="18"
+                         height="18"
+                         viewBox="0 0 24 24"
+                         fill="none"
+                         stroke="currentColor"
+                         stroke-width="1.5"
+                         stroke-linecap="round"
+                         stroke-linejoin="round"
+                         class="lucide lucide-arrow-down text-zinc-400 duration-300 group-hover:translate-y-1.5 group-hover:text-zinc-100">
+                        <path d="M12 5v14"></path>
+                        <path d="m19 12-7 7-7-7"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="perspective-[1500px] relative pt-16"
+         style="opacity: 1;">
+        <div id="tsparticles"
+             class="pointer-events-none absolute -top-36 left-1/2 h-[32rem] w-full -translate-x-1/2 -translate-y-1/2 overflow-hidden lg:w-[60rem]">
+            <canvas data-generated="true"
+                    style="width: 100% !important; height: 100% !important; margin: 0px !important; padding: 0px !important; position: fixed !important; z-index: 0 !important; top: 0px !important; left: 0px !important;"
+                    aria-hidden="true"
+                    height="512"
+                    width="960">
+            </canvas>
+        </div>
+
+        <div class="relative"
+             style="transform: none;">
+            <div
+                 class="absolute -top-px right-20 h-2 w-20 [mask-image:linear-gradient(to_right,rgba(217,217,217,0)_0%,#d9d9d9_25%,#d9d9d9_75%,rgba(217,217,217,0)_100%)] md:w-32 lg:w-64">
+                <div
+                     class="animate-starlight-right h-px w-full bg-gradient-to-r from-cyan-400/0 via-cyan-400 to-cyan-400/0">
+                </div>
+            </div>
+
+            <div class="rounded-md bg-zinc-950 ring-1 ring-white/10 lg:rounded-2xl">
+                <div class=""
+                     style="opacity: 1;">
+                    <img alt="App screenshot"
+                         fetchpriority="high"
+                         width="4200"
+                         height="2490"
+                         decoding="async"
+                         data-nimg="1"
+                         class="rounded-md lg:rounded-2xl"
+                         style="color:transparent"
+                         src="{{ asset('dashboard.svg') }}">
+                </div>
+            </div>
+
+            <div
+                 class="absolute -bottom-2 left-20 h-2 w-20 [mask-image:linear-gradient(to_right,rgba(217,217,217,0)_0%,#d9d9d9_25%,#d9d9d9_75%,rgba(217,217,217,0)_100%)] md:w-32 lg:w-64">
+                <div
+                     class="animate-starlight-left h-px w-full bg-gradient-to-r from-cyan-400/0 via-cyan-400 to-cyan-400/0">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
