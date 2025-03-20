f0eeaa6583527cf1bbb4a6153cfdf2e94589cf27
CharrafiMed <mhch5003@gmail.com>
2025-03-20 23:23:24 +0100

    add product prices migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,15 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'med',
             'email' => 'med@convergephp.com',
         ]);
+
         User::factory()->create([
             'name' => 'ayoub',
             'email' => 'ayoub@convergephp.com',
         ]);
+
+        Product::factory()->create([
+            'name' => 'blade components toolkit',
+            'slug' => 'blade-components-toolkit',
+            'description' => 'take you documentation to the next level with +20 pre-built blade components designd exclusivly for documentation and blogging plaforms',
+        ]);
+
+        
     }
 }
