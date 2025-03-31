24d093d63b969331acabfb331f94f7f8ae04b4f1
Ayoubhj866 <ayoubelhajji866@gmail.com>
2025-03-31 18:52:35 +0000

    install iconsax blade icons and use it for board links

--- resources/views/components/settings/link.blade.php
+++ resources/views/components/settings/link.blade.php
@@ -2,14 +2,15 @@
     'label' => null,
     'href' => null,
     'active' => false,
+    'icon' => 'iconsax-bul-card',
 ])
 
 <a {{ $attributes->whereStartsWith('wire:navigate') }}
    @class([
-       'bg-transparent py-3 border-l border-transparent px-4  bg-transparent text-start ',
-       'text-primary !border-primary ' => $active,
+       'bg-transparent py-3 border-l border-transparent px-2  bg-transparent text-start flex items-center gap-2  ',
+       'text-primary' => $active,
    ])
    href="{{ $href }}">
-
+    @svg("$icon", 'h-5 w-5')
     {{ $label }}
 </a>
