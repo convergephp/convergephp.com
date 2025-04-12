8fc0990b90e462776777adf707802843740af99a
CharrafiMed <mhch5003@gmail.com>
2025-04-12 15:32:59 +0200

    wip

--- app/Providers/Converge/DocsModuleProvider.php
+++ app/Providers/Converge/DocsModuleProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers\Converge;
 
+use Fluxtor\Converge\Enums\Layout;
+use Fluxtor\Converge\Enums\Spotlight;
 use Fluxtor\Converge\Module;
 use Fluxtor\Converge\Providers\ModuleProvider;
+use Fluxtor\Converge\Support\SidebarItemsStyles;
+use Fluxtor\Converge\Theme\Theme;
 
 class DocsModuleProvider extends ModuleProvider
 {
@@ -16,7 +20,19 @@ class DocsModuleProvider extends ModuleProvider
             ->id('converge-docs')
             ->default()
             ->routePath('docs')
-            ->in('docs');
+            ->theme(fn(Theme $theme) => $this->theme($theme))
+            ->in(base_path('docs'));
+    }
+
+    private function theme(Theme $theme)
+    {
+        return $theme
+            ->sidebarItemStyle(SidebarItemsStyles::STYLE4)
+            // ->highlighterTheme(HighlighterName::Vesper)
+            ->layout(Layout::Default)
+            ->collapsedGroupes(false)
+            ->font('inter')
+            ->spotlight(Spotlight::Strock);
     }
 }
 
