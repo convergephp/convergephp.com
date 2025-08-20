b0599d5abab8d55905bf549926834197205b1c26
charrafimed <mhch5003@gmail.com>
2025-08-20 21:46:50 +0200

    wip

--- resources/views/components/partials/sponsors.blade.php
+++ resources/views/components/partials/sponsors.blade.php
@@ -1,201 +1,59 @@
-<section x-data="{}"
-         class="mx-auto w-full max-w-7xl space-y-24 px-5 py-10 pt-10">
-    <div class="mx-auto grid w-full gap-y-12">
+<section
+    x-data="{}"
+    class="mx-auto w-full max-w-7xl space-y-24 px-5 py-10 pt-10"
+>
+        <div class="w-full overflow-hidden rounded-lg">
+            <div data-aos="fade-up"
+                 data-aos-duration="1000"
+                 class="mx-auto grid max-w-xl items-start justify-center gap-5 text-center">
+                <div class="text-4xl/[1.07] font-bold tracking-tight md:text-5xl/[1.07]">
+                    <span
+                          class="from-base-content bg-gradient-to-br to-gray-500 bg-clip-text tracking-widest text-transparent">
+                        Sponsors
+                    </span>
+                </div>
+                <div class="text-base-content/50  tracking-wide">
+                    This is What help us continue maintaing this project
+                </div>
+            </div>
+        </div>
+        <div class="mx-auto grid w-full gap-y-12">
+        
         <div class="text-dolphin col-span-full flex flex-wrap justify-around gap-x-10 gap-y-12">
-            {{-- <a class="grid grid-cols-2 place-items-center transition duration-300 will-change-transform hover:scale-110 motion-reduce:transition-none motion-reduce:hover:transform-none min-[850px]:col-span-2"
+            <a class="flex items-center gap-2  transition duration-300 will-change-transform hover:scale-110 motion-reduce:transition-none motion-reduce:hover:transform-none min-[850px]:col-span-2"
                href="https://fluxtor.dev/"
                target="_blank"
                title="Fluxtor">
-                <svg class="text-primary"
-                     xmlns="http://www.w3.org/2000/svg"
-                     width="30"
-                     fill="none"
-                     viewBox="0 0 88 88"
-                     height="30">
-                    <g clip-path="url(#a)">
-                        <g filter="url(#b)"
-                           shape-rendering="crispEdges">
-                            <path fill="url(#c)"
-                                  d="m51 40 7 4 7-4v-8l-7-4-7 4v8z"></path>
-                            <path fill="url(#d)"
-                                  d="m51 56 7 4 7-4v-8l-7-4-7 4v8z"></path>
-                            <path fill="url(#e)"
-                                  d="m37 64 7 4 7-4v-8l-7-4-7 4v8z"></path>
-                            <path fill="url(#f)"
-                                  d="m23 56 7 4 7-4v-8l-7-4-7 4v8z"></path>
-                            <path fill="url(#g)"
-                                  d="m23 40 7 4 7-4v-8l-7-4-7 4v8z"></path>
-                            <path fill="url(#h)"
-                                  d="m37 32 7 4 7-4v-8l-7-4-7 4v8z"></path>
-                        </g>
-                    </g>
-                    <defs>
-                        <linearGradient id="c"
-                                        x1="44"
-                                        x2="44"
-                                        y1="20"
-                                        y2="68"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#9F7AEA"
-                                  stop-opacity=".8"></stop>
-                            <stop offset="1"
-                                  stop-color="#9F7AEA"></stop>
-                        </linearGradient>
-                        <linearGradient id="d"
-                                        x1="44"
-                                        x2="44"
-                                        y1="20"
-                                        y2="68"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#9F7AEA"
-                                  stop-opacity=".8"></stop>
-                            <stop offset="1"
-                                  stop-color="#9F7AEA"></stop>
-                        </linearGradient>
-                        <linearGradient id="e"
-                                        x1="44"
-                                        x2="44"
-                                        y1="20"
-                                        y2="68"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#9F7AEA"
-                                  stop-opacity=".8"></stop>
-                            <stop offset="1"
-                                  stop-color="#9F7AEA"></stop>
-                        </linearGradient>
-                        <linearGradient id="f"
-                                        x1="44"
-                                        x2="44"
-                                        y1="20"
-                                        y2="68"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#9F7AEA"
-                                  stop-opacity=".8"></stop>
-                            <stop offset="1"
-                                  stop-color="#9F7AEA"></stop>
-                        </linearGradient>
-                        <linearGradient id="g"
-                                        x1="44"
-                                        x2="44"
-                                        y1="20"
-                                        y2="68"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#9F7AEA"
-                                  stop-opacity=".8"></stop>
-                            <stop offset="1"
-                                  stop-color="#9F7AEA"></stop>
-                        </linearGradient>
-                        <linearGradient id="h"
-                                        x1="44"
-                                        x2="44"
-                                        y1="20"
-                                        y2="68"
-                                        gradientUnits="userSpaceOnUse">
-                            <stop stop-color="#9F7AEA"
-                                  stop-opacity=".8"></stop>
-                            <stop offset="1"
-                                  stop-color="#9F7AEA"></stop>
-                        </linearGradient>
-                        <clipPath id="a">
-                            <path fill="#9F7AEA"
-                                  d="M0 88h88V0H0z"></path>
-                        </clipPath>
-                        <filter id="b"
-                                width="58"
-                                height="80"
-                                x="15"
-                                y="16"
-                                color-interpolation-filters="sRGB"
-                                filterUnits="userSpaceOnUse">
-                            <feFlood flood-opacity="0"
-                                     result="BackgroundImageFix"></feFlood>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset></feOffset>
-                            <feComposite in2="hardAlpha"
-                                         operator="out"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.521569 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.52 0">
-                            </feColorMatrix>
-                            <feBlend in2="BackgroundImageFix"
-                                     result="effect1_dropShadow_732_11578"></feBlend>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset dy="1"></feOffset>
-                            <feGaussianBlur stdDeviation="1.5"></feGaussianBlur>
-                            <feComposite in2="hardAlpha"
-                                         operator="out"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.706902 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.51 0">
-                            </feColorMatrix>
-                            <feBlend in2="effect1_dropShadow_732_11578"
-                                     result="effect2_dropShadow_732_11578"></feBlend>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset dy="5"></feOffset>
-                            <feGaussianBlur stdDeviation="2.5"></feGaussianBlur>
-                            <feComposite in2="hardAlpha"
-                                         operator="out"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.660078 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.44 0">
-                            </feColorMatrix>
-                            <feBlend in2="effect2_dropShadow_732_11578"
-                                     result="effect3_dropShadow_732_11578"></feBlend>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset dy="11"></feOffset>
-                            <feGaussianBlur stdDeviation="3.5"></feGaussianBlur>
-                            <feComposite in2="hardAlpha"
-                                         operator="out"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.566431 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.26 0">
-                            </feColorMatrix>
-                            <feBlend in2="effect3_dropShadow_732_11578"
-                                     result="effect4_dropShadow_732_11578"></feBlend>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset dy="20"></feOffset>
-                            <feGaussianBlur stdDeviation="4"></feGaussianBlur>
-                            <feComposite in2="hardAlpha"
-                                         operator="out"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.706902 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.08 0">
-                            </feColorMatrix>
-                            <feBlend in2="effect4_dropShadow_732_11578"
-                                     result="effect5_dropShadow_732_11578"></feBlend>
-                            <feBlend in="SourceGraphic"
-                                     in2="effect5_dropShadow_732_11578"
-                                     result="shape"></feBlend>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset dy="-4"></feOffset>
-                            <feGaussianBlur stdDeviation="6"></feGaussianBlur>
-                            <feComposite in2="hardAlpha"
-                                         k2="-1"
-                                         k3="1"
-                                         operator="arithmetic"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.521569 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.25 0">
-                            </feColorMatrix>
-                            <feBlend in2="shape"
-                                     result="effect6_innerShadow_732_11578"></feBlend>
-                            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                                           in="SourceAlpha"
-                                           result="hardAlpha"></feColorMatrix>
-                            <feOffset dy="-1"></feOffset>
-                            <feGaussianBlur stdDeviation=".5"></feGaussianBlur>
-                            <feComposite in2="hardAlpha"
-                                         k2="-1"
-                                         k3="1"
-                                         operator="arithmetic"></feComposite>
-                            <feColorMatrix values="0 0 0 0 0.521569 0 0 0 0 0.129412 0 0 0 0 0.909804 0 0 0 0.4 0">
-                            </feColorMatrix>
-                            <feBlend in2="effect6_innerShadow_732_11578"
-                                     result="effect7_innerShadow_732_11578"></feBlend>
-                        </filter>
-                    </defs>
-                </svg> <span class="font-bold">Fluxtor</span>
-            </a> --}}
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 100 100"
+                    class="size-7">
+                    <rect x="15"
+                        y="10"
+                        width="80"
+                        height="15"
+                        fill="currentColor"
+                        rx="5"
+                        ry="0" />
+
+                    <rect x="15"
+                        y="30"
+                        width="60"
+                        height="15"
+                        fill="currentColor" />
+
+                    <rect x="15"
+                        y="50"
+                        width="30"
+                        height="15"
+                        fill="currentColor" />
+                    <rect x="15"
+                        y="55"
+                        width="10"
+                        height="30"
+                        fill="currentColor" />
+                </svg>
+                 <span class="font-bold text-3xl ">Fluxtor</span>
+            </a>
         </div>
 
         {{-- <div class="flex justify-center">
